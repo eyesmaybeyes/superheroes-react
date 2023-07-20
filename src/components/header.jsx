9c8f88b9f7914a3a831a815264788e0d0ec6c542
Maria <114153517+eyesmaybeyes@users.noreply.github.com>
2023-07-20 13:42:55 +0300

    fixed jsx and added gif

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,20 @@
-import '../styles/style.css'
+import "../styles/style.css";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
-            <header className="header">
-                <a className="header__link_marvel" href="https://www.marvel.com/">Marvel</a>
-                <p className="header-title">Best Superheroes of All-Time</p>
-                <a className="header__link_dc" href="https://www.dc.com/">DC</a>
-            </header>
-    )
+        <header className="header">
+            <a className="header__link_marvel" href="https://www.marvel.com/">
+                Marvel
+            </a>
+            <Link to={`/`} className="header-title">
+                Best Superheroes of All-Time
+            </Link>
+            <a className="header__link_dc" href="https://www.dc.com/">
+                DC
+            </a>
+        </header>
+    );
 }
 
-export {Header};
+export { Header };
